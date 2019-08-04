daff7d29d9b558603785bcb882f11e1e1f8c1461
AMOREM <amorem@amorem.com>
2019-08-04 18:57:02 +0200

    Passing data using the constructors (like props in React)

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoryMeals extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMeals(this.categoryId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("The Recipies"),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text('The recipies for the category'),
